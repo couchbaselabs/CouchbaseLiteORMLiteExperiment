c89d158c3a80e73b01dd8e210b6e456db04e846d
Traun Leyden <traun@couchbase.com>
2013-09-16 15:38:17 -0700

    use android studio tool to generate activity

--- ToDoLiteAndroid/src/main/java/com/example/todoliteandroid/MainActivity.java
+++ ToDoLiteAndroid/src/main/java/com/example/todoliteandroid/MainActivity.java
@@ -1,19 +1,17 @@
 package com.example.todoliteandroid;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.app.Activity;
 import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.View;
 import android.view.Window;
 import android.view.inputmethod.EditorInfo;
-import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Arrays;
