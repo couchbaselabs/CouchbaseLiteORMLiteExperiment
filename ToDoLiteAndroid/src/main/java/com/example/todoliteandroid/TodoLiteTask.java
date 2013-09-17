359e6c1a83cfef4aa0c1bfc2ab84cd627e205666
Traun Leyden <traun@couchbase.com>
2013-09-17 14:30:26 -0700

    custom view adapter with reactive checkbox

--- ToDoLiteAndroid/src/main/java/com/example/todoliteandroid/TodoLiteTask.java
+++ ToDoLiteAndroid/src/main/java/com/example/todoliteandroid/TodoLiteTask.java
@@ -3,9 +3,26 @@ package com.example.todoliteandroid;
 public class TodoLiteTask {
 
     private String name;
+    private boolean isSelected;
 
     public TodoLiteTask(String name) {
         this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isSelected() {
+        return isSelected;
+    }
+
+    public void setSelected(boolean selected) {
+        isSelected = selected;
     }
 
     @Override
--- ToDoLiteAndroid/src/main/java/com/example/todoliteandroid/TodoLiteTaskArrayAdapter.java
+++ ToDoLiteAndroid/src/main/java/com/example/todoliteandroid/TodoLiteTaskArrayAdapter.java
@@ -1,13 +1,71 @@
 package com.example.todoliteandroid;
 
+import android.app.Activity;
 import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.TextView;
 
 import java.util.List;
 
 public class TodoLiteTaskArrayAdapter extends ArrayAdapter<TodoLiteTask> {
 
+    private final List<TodoLiteTask> list;
+    private final Context context;
+
     public TodoLiteTaskArrayAdapter(Context context, int resource, int textViewResourceId, List<TodoLiteTask> objects) {
         super(context, resource, textViewResourceId, objects);
+        this.context = context;
+        this.list = objects;
     }
+
+    static class ViewHolder {
+        protected TextView text;
+        protected CheckBox checkbox;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View view = null;
+        if (convertView == null) {
+            Activity activity = (Activity) context;
+            LayoutInflater inflator = activity.getLayoutInflater();
+            view = inflator.inflate(R.layout.layout_taskrow, null);
+            final ViewHolder viewHolder = new ViewHolder();
+            viewHolder.text = (TextView) view.findViewById(R.id.taskRowLabel);
+            viewHolder.checkbox = (CheckBox) view.findViewById(R.id.checkBox);
+            viewHolder.checkbox
+                    .setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+
+                        @Override
+                        public void onCheckedChanged(CompoundButton buttonView,
+                                                     boolean isChecked) {
+                            TodoLiteTask todoLiteTask = (TodoLiteTask) viewHolder.checkbox
+                                    .getTag();
+                            todoLiteTask.setSelected(isChecked);
+
+
+                        }
+                    });
+            view.setTag(viewHolder);
+            viewHolder.checkbox.setTag(list.get(position));
+        } else {
+            view = convertView;
+            ((ViewHolder) view.getTag()).checkbox.setTag(list.get(position));
+        }
+
+        ViewHolder holder = (ViewHolder) view.getTag();
+        TodoLiteTask todoLiteTask = list.get(position);
+        String taskName = todoLiteTask.toString();
+        holder.text.setText(taskName);
+        holder.checkbox.setChecked(todoLiteTask.isSelected());
+        return view;
+
+    }
+
+
 }
